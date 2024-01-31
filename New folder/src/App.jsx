bc28b1b44f873a7915679fde32d1069511119fbf
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-31 21:04:27 +0500

    first and second componet

---
+++ New folder/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import First from './components/firstComponent/first';
+import Second from './components/secondComponent/second';
+function App() {
+  return (
+    <div className="main">
+
+    <div className="App">
+      <h1>this is my react page</h1>
+    </div>
+      <First />
+      <Second />
+    </div>
+  );
+}
+
+export default App;
